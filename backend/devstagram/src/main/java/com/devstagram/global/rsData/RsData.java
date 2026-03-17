5b3fcb88b19a7bee7f5ef9282776bf0fb9ca3027
kimdohwan7220 <dohwan8220@naver.com>
2026-03-17 09:39:05 +0900

    Feat: 공통 응답 규격 RsData 구현 및 정적 팩토리 메서드 추가

---
+++ backend/devstagram/src/main/java/com/devstagram/global/rsData/RsData.java
@@ -0,0 +1,36 @@
+package com.devstagram.global.rsData;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public record RsData<T>(
+        String resultCode,
+        @JsonIgnore
+        int statusCode,
+        String msg,
+        T data
+) {
+    public static <T> RsData<T> success(T data) {
+        return new RsData<>("200-S-1", "성공", data);
+    }
+
+    public static <T> RsData<T> success(String msg, T data) {
+        return new RsData<>("200-S-1", msg, data);
+    }
+
+    public static <T> RsData<T> success() {
+        return success(null);
+    }
+
+    public static <T> RsData<T> fail(String msg) {
+        return new RsData<>("400-F-1", msg, null);
+    }
+
+    @JsonIgnore
+    public boolean isSuccess() {
+        return statusCode >= 200 && statusCode < 400;
+    }
+
+    public RsData(String resultCode, String msg, T data) {
+        this(resultCode, Integer.parseInt(resultCode.split("-", 2)[0]), msg, data);
+    }
+}
