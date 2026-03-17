7bf7e708c31afd1de36b7faf72f5400a32c78567
kimdohwan7220 <dohwan8220@naver.com>
2026-03-17 12:09:47 +0900

    feat: user, userinfo, gender, resume, h2 콘솔 확인을 위한 buildgradle과 securityconfig 추가
    # Conflicts:
    #	backend/devstagram/build.gradle.kts

--- backend/devstagram/src/main/java/com/devstagram/domain/user/entity/User.java
+++ backend/devstagram/src/main/java/com/devstagram/domain/user/entity/User.java
@@ -1,12 +1,55 @@
 package com.devstagram.domain.user.entity;
 
-import java.util.Date;
+import com.devstagram.domain.user.entity.Gender;
+import com.devstagram.domain.user.entity.UserInfo;
+import com.devstagram.global.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public class User {
-    String id;
-    String email;
-    String password;
-    Date birthDate;
-    Gender gender;
-    Date createdAt;
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class User extends BaseEntity { // 팀원이 만든 BaseEntity 상속!
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING) // DB에 문자열로 저장
+    @Column(nullable = false)
+    private Gender gender;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserInfo userInfo;
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+        userInfo.setUser(this);
+    }
 }
