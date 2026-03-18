2bd69c05c22e7fa9f1bf5010e2461ab927ba1cb3
NohGar <njwwn193@naver.com>
2026-03-18 11:13:20 +0900

    feat: 코드 스타일 적용

--- backend/devstagram/src/test/java/com/devstagram/domain/post/repository/PostRepositoryTest.java
+++ backend/devstagram/src/test/java/com/devstagram/domain/post/repository/PostRepositoryTest.java
@@ -1,16 +1,18 @@
 package com.devstagram.domain.post.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDateTime;
 import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
-import com.devstagram.domain.post.entity.Post;
 
+import com.devstagram.domain.post.entity.Post;
 
 @DataJpaTest
 class PostRepositoryTest {
