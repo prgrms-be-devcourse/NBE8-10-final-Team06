fddeb82ffed2863e911baaa6f3f223bf32720583
kimdohwan7220 <dohwan8220@naver.com>
2026-03-20 11:47:29 +0900

    refactor: 불필요한 빌더 패턴 제거

--- backend/devstagram/src/main/java/com/devstagram/domain/user/dto/FollowResponse.java
+++ backend/devstagram/src/main/java/com/devstagram/domain/user/dto/FollowResponse.java
@@ -1,8 +1,5 @@
 package com.devstagram.domain.user.dto;
 
-import lombok.Builder;
-
-@Builder
 public record FollowResponse(
         Long toUserId, // 팔로우/언팔로우 동작의 대상자(상대방) ID
         boolean isFollowing, // 현재 로그인한 '나'의 팔로우 상태
@@ -10,11 +7,6 @@ public record FollowResponse(
         long followingCount // '나(로그인 유저)'의 최신 팔로잉 총 숫자
         ) {
     public static FollowResponse of(Long toUserId, boolean isFollowing, long followerCount, long followingCount) {
-        return FollowResponse.builder()
-                .toUserId(toUserId)
-                .isFollowing(isFollowing)
-                .followerCount(followerCount)
-                .followingCount(followingCount)
-                .build();
+        return new FollowResponse(toUserId, isFollowing, followerCount, followingCount);
     }
 }
