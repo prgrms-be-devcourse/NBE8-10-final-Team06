52358ba5e00b0c80287e69072faf5aac98db6cb2
NohGar <njwwn193@naver.com>
2026-03-20 15:22:21 +0900

    fix: Comment 응답폼 내 id, authorId 추가

--- backend/devstagram/src/main/java/com/devstagram/domain/comment/dto/CommentInfoRes.java
+++ backend/devstagram/src/main/java/com/devstagram/domain/comment/dto/CommentInfoRes.java
@@ -5,16 +5,18 @@ import java.time.LocalDateTime;
 import com.devstagram.domain.comment.entity.Comment;
 
 public record CommentInfoRes(
+        Long id,
+        Long userId,
         String content,
-        long userId,
         String nickname,
         LocalDateTime createdAt,
         LocalDateTime modifiedAt,
         long replyCount) {
     public CommentInfoRes(Comment comment) {
         this(
-                comment.getContent(),
+                comment.getId(),
                 comment.getUser().getId(),
+                comment.getContent(),
                 comment.getUser().getNickname(),
                 comment.getCreatedAt(),
                 comment.getModifiedAt(),
--- backend/devstagram/src/main/java/com/devstagram/domain/comment/dto/ReplyInfoRes.java
+++ backend/devstagram/src/main/java/com/devstagram/domain/comment/dto/ReplyInfoRes.java
@@ -5,11 +5,12 @@ import java.time.LocalDateTime;
 import com.devstagram.domain.comment.entity.Comment;
 
 public record ReplyInfoRes(
-        String content, long userId, String nickname, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        Long id, Long userId, String content, String nickname, LocalDateTime createdAt, LocalDateTime modifiedAt) {
     public ReplyInfoRes(Comment comment) {
         this(
-                comment.getContent(),
+                comment.getId(),
                 comment.getUser().getId(),
+                comment.getContent(),
                 comment.getUser().getNickname(),
                 comment.getCreatedAt(),
                 comment.getModifiedAt());
